abfae940bd5568e1d87aabf9ec917623eb2fcc54
avbatchelor <alexbatchelor@hotmail.co.uk>
2016-08-26 15:05:31 -0400

    new stim set for kir experiments

---
+++ ballAcq/stimSets/ballStimSet_007.m
@@ -0,0 +1,67 @@
+function ballStimSet_007(exptInfo)
+
+% Produces the default pip train while switching between all three speakers
+
+%% Archive this code
+archiveExpCodeBall(exptInfo)
+
+%% Set up and acquire with the stimulus set
+numberOfStimuli = 8;
+trialMeta.totalStimNum = numberOfStimuli;
+trialsPerBlock = numberOfStimuli;
+speakerNonRan = repmat(1:numberOfStimuli,1,trialsPerBlock/numberOfStimuli);
+stimRan = speakerNonRan(randperm(trialsPerBlock));
+
+count = 1;
+stop = 0;
+while stop == 0
+    trialMeta.pauseDur = rand(1,1);
+    pause on 
+    pause(trialMeta.pauseDur);
+    trialMeta.stimNum = stimRan(count);
+    pickStimNum = round(trialMeta.stimNum/2);
+    stim = pickStimulus(pickStimNum);
+    if mod(trialMeta.stimNum,2) == 1
+        stim.speaker = 1; 
+    else 
+        stim.speaker = 3; 
+    end
+    trialMeta.outputCh = switchSpeakerBall(stim.speaker);
+    acquireBallTrial(stim,exptInfo,trialMeta);
+    if count == trialsPerBlock
+        count = 1;
+        stimRan = speakerNonRan(randperm(trialsPerBlock));
+    else
+        count = count+1;
+    end
+end
+
+    function stim = pickStimulus(stimNum)
+        switch stimNum
+            case 1
+                stim = PipStimulus;
+                stim.maxVoltage = 1.5;
+                stim.startPadDur = 2;
+                stim.endPadDur = 2;
+            case 2
+                stim = PulseSong;
+                stim.maxVoltage = 1.5;
+                stim.startPadDur = 2;
+                stim.endPadDur = 2;
+            case 3
+                stim = SineWave;
+                stim.carrierFreqHz = 35;
+                stim.maxVoltage = 1.5;
+                stim.startPadDur = 2;
+                stim.endPadDur = 2;
+            case 4
+                stim = AmTone;
+                stim.carrierFreqHz = 300;
+                stim.modFreqHz = 16;
+                stim.maxVoltage = 1.5;
+                stim.startPadDur = 2;
+                stim.endPadDur = 2;
+        end
+    end
+
+end
