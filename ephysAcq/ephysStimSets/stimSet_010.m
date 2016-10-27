91f9cba4d9d7dfd954fa1fe92afb6247f8d12efb
avbatchelor <alexbatchelor@hotmail.co.uk>
2016-10-27 11:12:25 -0400

    changed stim volumes

--- ephysAcq/ephysStimSets/stimSet_010.m
+++ ephysAcq/ephysStimSets/stimSet_010.m
@@ -34,31 +34,39 @@ switch stimNum
     case 1
         stim = PipStimulus;
         stim.speaker = 2;
+        stim.maxVoltage = 0.025;
     case 2
         stim = Chirp;
         stim.speaker = 2;
+        stim.maxVoltage = 0.025;
     case 3
         stim = Chirp;
         stim.startFrequency  = 1500;
         stim.endFrequency    = 90;
+        stim.maxVoltage = 0.025;
     case 4
         stim = CourtshipSong;
         stim.speaker = 2;
+        stim.maxVoltage = 0.025;
     case 5
         stim = PulseSong;
         stim.speaker = 2;
+        stim.maxVoltage = 0.025;
     case 6
         stim = ClickStimulus;
         stim.speaker = 2;
+        stim.maxVoltage = 0.025;
     case num2cell(7:17)
         stimNumStart = 7;
         stim = SineWave;
         stim.carrierFreqHz = 25*sqrt(2)^((stimNum-stimNumStart)+1);
+        stim.maxVoltage = 0.025;
     case num2cell(18:22)
         stimNumStart = 18;
         stim = AmTone;
         stim.carrierFreqHz = 300;
         stim.modFreqHz = 2^(stimNum - stimNumStart);
+        stim.maxVoltage = 0.025;
 end
 end
 
--- ephysAcq/ephysStimSets/stimSet_019.m
+++ ephysAcq/ephysStimSets/stimSet_019.m
@@ -39,31 +39,34 @@ clear FS ;
                 stim = SineWave;
                 stim.carrierFreqHz = 200;
                 stim.probe = 'left';
+                stim.maxVoltage = 0.0125;
             case 2
                 % Saturation test
                 stim = SineWave;
                 stim.carrierFreqHz = 200;
-                stim.maxVoltage = 0.2;
+                stim.maxVoltage = 0.00625;
                 stim.probe = 'left';
             case 3
                 stim = SineWave;
                 stim.carrierFreqHz = 200;
                 stim.probe = 'off';
+                stim.maxVoltage = 0.0125;
             case 4
                 % Saturation test
                 stim = SineWave;
                 stim.carrierFreqHz = 200;
-                stim.maxVoltage = 0.2;
+                stim.maxVoltage = 0.00625;
                 stim.probe = 'off';
             case 5
                 stim = SineWave;
                 stim.carrierFreqHz = 200;
                 stim.probe = 'right';
+                stim.maxVoltage = 0.0125;
             case 6
                 % Saturation test
                 stim = SineWave;
                 stim.carrierFreqHz = 200;
-                stim.maxVoltage = 0.2;
+                stim.maxVoltage = 0.00625;
                 stim.probe = 'right';
         end
     end
